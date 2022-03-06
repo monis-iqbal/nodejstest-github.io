e3442114534b1dfb05e73d522fea74c37bf73166
monis-iqbal <monisfunter7@gmail.com>
2022-03-06 16:37:49 +0500

    nodejstest
    test

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    amount: {
+        type: Number,
+        validate: {
+            validator: (val) => {
+                return val > 0;
+            },
+            message: "amount cannot be less then 0."
+        }
+    },
+    products: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Product"
+        }
+    ]
+});
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
